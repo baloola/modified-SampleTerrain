6f900a774c4afde6c768b94511a578b06dc81423
baloola <baloola-mu@hotmail.com>
2017-10-21 23:47:33 +0200

    modiied from a modified

--- sampleterrain.js
+++ sampleterrain.js
@@ -8,13 +8,13 @@ console.warn('Sampling terrain...');
 
 var options = cli.parse({
     file: ['f', 'GeoJson File', 'file'],
-    level: ['l', 'Terrain level of detail', 'int', 11]
+   
 });
 
 if (options.file) {
   fs.readFile(options.file, 'utf8', function(err, data) {
     if (err) {throw err;}
-    sampleterrain.sample(JSON.parse(data), options.level, function(e, o) {
+    sampleterrain.sample(JSON.parse(data), function(e, o) {
       if (e) {
         console.warn('Sample terrain error', e);
       } else {
